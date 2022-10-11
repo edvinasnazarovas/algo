987b70eea62737d4c8b6de9faa5438d050f2a741
Edvinas <93229121+edvinasnazarovas@users.noreply.github.com>
2022-10-11 22:36:36 +0300

    Update lab_1.c

--- laboratorium_1/lab_1.c
+++ laboratorium_1/lab_1.c
@@ -1,4 +1,4 @@
-Obliczyc funkcja y = ax^2 znaczenia, gdy a ir x zmeniajan sie jednoczesnie: a z 1 krokiem 0.5, x - z 2 do 6 krokiem 2
+//Obliczyc funkcja y = ax^2 znaczenia, gdy a ir x zmeniajan sie jednoczesnie: a z 1 krokiem 0.5, x - z 2 do 6 krokiem 2
 
 #include <stdio.h>
 #include <math.h>
