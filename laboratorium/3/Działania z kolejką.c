4910a6261287ff22384a2cc8ee6f056297bbbdd1
Edvinas <93229121+edvinasnazarovas@users.noreply.github.com>
2022-11-12 04:18:15 -0800

    Update Działania z kolejką.c

--- laboratorium/3/Działania z kolejką.c
+++ laboratorium/3/Działania z kolejką.c
@@ -12,7 +12,7 @@ struct Item {
 
 int printList(struct Item *head);
 
-void insert(float item, struct Item** end);
+void insert(float item, struct Item **end);
 
 void removeItem(int pos, struct Item **head);
 
@@ -36,7 +36,7 @@ void main(void) {
   insert(4, &end);
   insert(5, &end);
   insert(6, &end);
-  
+
   removeItem(0, &begin);
 
   printList(begin);
@@ -46,34 +46,26 @@ void main(void) {
 
 // Funkcii
 
-void insert(float item, struct Item** end)
-{
-    /* 1. allocate node */
-    struct Item* new_node = (struct Item*) malloc(sizeof(struct Item));
-  
-    struct Item *last = *end;  /* used in step 5*/
-   
-    /* 2. put in the data  */
-    new_node->value  = item;
-  
-    /* 3. This new node is going to be the last node, so make next 
-          of it as NULL*/
-    new_node->next = NULL;
-  
-    /* 4. If the Linked List is empty, then make the new node as head */
-    if (*end == NULL)
-    {
-       *end = new_node;
-       return;
-    }  
-       
-    /* 5. Else traverse till the last node */
-    while (last->next != NULL)
-        last = last->next;
-   
-    /* 6. Change the next of last node */
-    last->next = new_node;
-    return;    
+void insert(float item, struct Item **end) {
+
+  struct Item *new_node = (struct Item *)malloc(sizeof(struct Item));
+
+  struct Item *last = *end;
+
+  new_node->value = item;
+
+  new_node->next = NULL;
+
+  if (*end == NULL) {
+    *end = new_node;
+    return;
+  }
+
+  while (last->next != NULL)
+    last = last->next;
+
+  last->next = new_node;
+  return;
 }
 
 void removeItem(int pos, struct Item **head) {
