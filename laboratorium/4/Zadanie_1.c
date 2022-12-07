0e88ca3ee4edccf3619cb27c7797a4f06dab8cf6
Edvinas <93229121+edvinasnazarovas@users.noreply.github.com>
2022-12-07 05:27:10 -0800

    Update Zadanie_1.c

--- laboratorium/4/Zadanie_1.c
+++ laboratorium/4/Zadanie_1.c
@@ -1,19 +1,41 @@
 #include <stdio.h>
- 
-long Factorial(int);
- 
-int main()
-{
-  printf("%d", pascal(7, 4));
+
+int myFunction(int n, int a);
+
+int main(void) {
+  int n = 4;
+  int a = n;
+  printf("a: %d\n", myFunction(n, a));
   return 0;
 }
 
-int pascal(int row, int col) {
-   if(col == 0)
-     return 1;
+int myFunction(int n, int a) {
+  if(a == 0)
+    return 0;
+  
+  int k1 = 2;
+  int k2 = 2;
 
-  if(row == 0)
-    return col;
-
-  return (row * pascal(row - 1, col - 1)) / col;
+  int k1_pow = 1;
+  int k2_pow = 1;
+  
+  for(int i = 0; i < a - 1; i++)
+    {
+      k1_pow = k1_pow * k1;
+      printf("i: %d k1: %d\n", i, k1_pow);
+    }
+    
+  for(int i = 0; i < a; i++)
+    {
+      k2_pow = k2_pow * k2;
+    }
+    
+  if(k1_pow <= n && k2_pow > n)
+  {
+    printf("%d  <=  %d  <= %d\n", k1_pow, n, k2_pow);
+    return a;
+  }
+  else 
+    return myFunction(n, a - 1);
+    
 }
