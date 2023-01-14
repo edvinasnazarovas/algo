92fa085c7bb0649cc3e5ca3dae8fd92df086e081
Edvinas <93229121+edvinasnazarovas@users.noreply.github.com>
2023-01-14 18:37:32 +0200

    Update lab_tablicy_1.c

--- laboratorium/1/lab_tablicy_1.c
+++ laboratorium/1/lab_tablicy_1.c
@@ -4,10 +4,7 @@
 //Obliczyc funkcja y = ax^2 znaczenia, gdy a ir x zmeniajan sie jednoczesnie: a z 1 krokiem 0.5, x - z 2 do 6 krokiem 2
 
 int main(void) {
-  double a, x;
-
-  a = 1;
-  x = 2;
+  double a = 1.0, x = 2.0;
 
   while(x <= 6){
     printf("%f = ", (a*x*x));
