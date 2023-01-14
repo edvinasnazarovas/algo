b013adf149b298fdc27c9df1bb5047d29c43fe73
Edvinas <93229121+edvinasnazarovas@users.noreply.github.com>
2023-01-14 22:01:43 +0200

    Update lab_tablicy_1.c

--- laboratorium/1/lab_tablicy_1.c
+++ laboratorium/1/lab_tablicy_1.c
@@ -1,7 +1,12 @@
+/*
+ * Nazwa pliku: lab_tablicy_1.c
+ * Autor: Edvinas
+ * Data: 2022-10-13
+ * Opis: Obliczyc funkcja y = ax^2 znaczenia, gdy a ir x zmeniajan sie jednoczesnie: a z 1 krokiem 0.5, x - z 2 do 6 krokiem 2
+ */
+
 #include <stdio.h>
 #include <math.h>
-
-//Obliczyc funkcja y = ax^2 znaczenia, gdy a ir x zmeniajan sie jednoczesnie: a z 1 krokiem 0.5, x - z 2 do 6 krokiem 2
 
 int main(void) {
   double a = 1.0, x = 2.0;
