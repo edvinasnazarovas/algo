184e4fe5b5bb8d08ee183414321b9a15b428b415
Edvinas <93229121+edvinasnazarovas@users.noreply.github.com>
2023-01-14 20:59:02 +0200

    Update Zadanie_3.c

--- laboratorium/4/Zadanie_3.c
+++ laboratorium/4/Zadanie_3.c
@@ -1,24 +1,26 @@
+/*
+ * Nazwa pliku: Zadanie_3.c
+ * Autor: Edvinas
+ * Data: 2022-11-22
+ * Opis: Napisz funkcja rekurencyjna do obliczenia n-tej liczby ciągu arytmcznego
+ */
+
 #include <stdio.h>
 
-int Arytm(int arr[], int n, int d);
+int arytm(int a, int d, int n);
 
 int main(void) {
   printf("Hello World\n");
-  int k[] = {1, 4, 7, 10};
-  int n = 3;
-  int d = k[n] - k[n-1];
-  printf("Arytm is: %d\n", Arytm(k, n, d));
+  int a = 1;
+  int d = 3;
+  int n = 7;
+  printf("Arytm is: %d\n", arytm(a, d, n));
   return 0;
 }
 
-int Arytm(int arr[], int n, int d) {
-  printf("%d\n", n);
-
-  if(n == 1) 
-    return d;
-  
-  if(arr[n] - arr[n-1] != d)
-    return 0;
-    
-  return Arytm(arr, n - 1, d);
+int arytm(int a, int d, int n) {
+  if (n == 1) {
+    return a;
+  }
+  return arytm(a, d, n - 1) + d;
 }
