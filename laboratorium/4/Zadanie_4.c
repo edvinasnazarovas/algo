979e0ccf7763c1d7c8cabf14775592d379919776
Edvinas <93229121+edvinasnazarovas@users.noreply.github.com>
2023-01-14 19:12:28 +0200

    Update Zadanie_4.c

--- laboratorium/4/Zadanie_4.c
+++ laboratorium/4/Zadanie_4.c
@@ -6,17 +6,16 @@ int main(void) {
   printf("Hello World\n");
   float sum = 0;
 
-  printf("Sum: %f\n", geo(1, 0.5, 10, sum));
+  geo(1, 0.5, 10)
   
   return 0;
 }
 
-float geo(float a, float r, int n, float sum) {
-  sum = sum + a;
+float geo(float a, float r, int n) {
   a = a * r;
   
   if(n == 1)
-    return sum;
+    return 0;
   
   else
     return geo(a, r, n - 1, sum);
