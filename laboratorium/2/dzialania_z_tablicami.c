afbd90872f859b100ee3fd1bb11fc6f745b1309f
Edvinas <93229121+edvinasnazarovas@users.noreply.github.com>
2023-01-15 18:24:48 +0200

    Update dzialania_z_tablicami.c

--- laboratorium/2/dzialania_z_tablicami.c
+++ laboratorium/2/dzialania_z_tablicami.c
@@ -1,90 +1,100 @@
-int find(int item, int *arr);
-
-void insert(int item, int pos, int *arr);
-
-void rem(int pos, int *arr);
-
-int size(int *arr);
-
-int findMax(int *arr);
-
-int findMin(int *arr);
-
-void printTable(int *arr);
+/*
+ * Nazwa pliku: dzialania_z_tablicami.c
+ * Autor: Edvinas
+ * Data: 2022-11-2
+ * Opis: Działanie z tablicami
+ */
 
 #include <stdio.h>
 #include <stdlib.h>
 
-void main(void) {
-  int n = 10;
-  int *arr = calloc(n, sizeof(int));
+int size;
 
-  for(int i = 0; i < n; i++)
-    scanf("%d", arr + i);
-  
-  // realizacja
-
-  return;
+int find(int *arr, int item) {
+  for (int i = 0; i < size; i++) {
+    if (arr[i] == item) {
+      return i;
+    }
+  }
+  return -1;
 }
 
-    // Funkcii
-
-int find(int item, int *arr){
-  int n = size(arr);
-  
-  while(n--)
-    if(arr[n] == item)
-      return n;
-}
-
-void insert(int item, int pos, int *arr){
-  int length = sizeof(*arr) / sizeof(int);
-  if (pos <= length) {
-    arr[pos] = item;
+void insert(int item, int pos, int **arr) {
+  if (pos < size) {
+    *arr = (int *)realloc(*arr, (size + 1) * sizeof(int));
+    for (int i = size - 1; i >= pos; i--) {
+      (*arr)[i + 1] = (*arr)[i];
+    }
+    (*arr)[pos] = item;
+    size++;
   }
 }
 
-void rem(int pos, int *arr){
-  int length = sizeof(*arr) / sizeof(int);
-  if(pos <= length){
-    arr[pos] = 0;
+void rem(int pos, int **arr) {
+  if (pos < size) {
+    for (int i = pos; i < size - 1; i++) {
+      (*arr)[i] = (*arr)[i + 1];
+    }
+    size--;
+    *arr = (int *)realloc(*arr, size * sizeof(int));
   }
 }
 
-
-int size(int *arr){
-  int length = 0;
-  while(arr[length] != 0)
-    length++;
-  return length - 1;
-}
-
-
-int findMax(int *arr){
+int findMax(int *arr) {
   int max = arr[0];
-  int n = size(arr);
-
-  while(n--){
-    if(arr[n] > max) max = arr[n];
+  for (int i = 1; i < size; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+    }
   }
-
   return max;
 }
 
-int findMin(int *arr){
+int findMin(int *arr) {
   int min = arr[0];
-  int n = size(arr);
-
-  while(n--){
-    if(arr[n] < min) min = arr[n];
+  for (int i = 1; i < size; i++) {
+    if (arr[i] < min) {
+      min = arr[i];
     }
+  }
   return min;
 }
 
-void printTable(int *arr){
-  int n = size(arr);
-
-  while(n--){
-    printf("%d %d\n", n, arr[n]);
+void printTable(int *arr) {
+  printf("\n");
+  for (int i = 0; i < size; i++) {
+    printf("[%d] %d\n", i, arr[i]);
   }
+}
+
+int main(void) {
+  int *arr = (int *)malloc(sizeof(int));
+  size = 1;
+
+  insert(5, 0, &arr);
+  insert(1, 1, &arr);
+  insert(2, 2, &arr);
+  insert(4, 3, &arr);
+  insert(5, 4, &arr);
+  insert(10, 5, &arr);
+
+  printTable(arr);
+
+  printf("size of the array: %d\n", size);
+
+  rem(2, &arr);
+
+  printTable(arr);
+
+  printf("size of the array: %d\n", size);
+
+  printf("Max: %d\n", findMax(arr));
+  printf("Min: %d\n", findMin(arr));
+
+  printf(find(arr, 4) != -1 ? "Found in pos %d\n" : "Not found\n",
+         find(arr, 4));
+
+  free(arr);
+
+  return 0;
 }
